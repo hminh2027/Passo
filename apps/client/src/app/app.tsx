494958d6f8dd6119bfb0a314d4544cca87c4acce
pta201 <pta20801@gmail.com>
2024-06-25 20:25:43 +0700

    feat (client): init

---
+++ apps/client/src/app/app.tsx
@@ -0,0 +1,35 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+import { Link, Route, Routes } from 'react-router-dom';
+
+export function App() {
+  return (
+    <div>
+      {/* START: routes */}
+      {/* These routes and navigation have been generated for you */}
+      {/* Feel free to move and update them to fit your needs */}
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              This is the generated root route. <Link to="/page-2">Click here for page 2.</Link>
+            </div>
+          }
+        />
+        <Route
+          path="/page-2"
+          element={
+            <div>
+              <Link to="/">Click here to go back to root page.</Link>
+            </div>
+          }
+        />
+      </Routes>
+      {/* END: routes */}
+    </div>
+  );
+}
+
+export default App;
